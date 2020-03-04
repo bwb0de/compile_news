606decaa44a88ec2a8e63dbf055718dfcc186774
Daniel Cruz <dftdcruz@gmail.com>
2020-03-04 12:15:59 -0300

    Refatoração de código e inclusão de links para busca nas notícias da UnB

--- modules/web_scraping_base.py
+++ modules/web_scraping_base.py
@@ -23,7 +23,11 @@ folha = 'https://www.folha.uol.com.br'
 br_de_fato = 'https://www.brasildefato.com.br/politica'
 correioweb_cidades = 'https://www.correiobraziliense.com.br/cidades---df/'
 correioweb_politica = 'https://www.correiobraziliense.com.br/politica/'
+unb_noticias = 'http://www.noticias.unb.br/'
+unb_ciencias = 'http://www.unbciencia.unb.br/ultimas'
 
+def convert_to_readaloud():
+    os.system('for FILE in $(ls); do echo "$FILE"; iconv -c -f utf-8 -t iso-8859-15 "$FILE" > "nv-$FILE"; done')
 
 
 def get_page(url):
@@ -40,34 +44,80 @@ def post_page(url):
         return
     return response.data
 
-def parse_url(url):
-    http_doc = get_page(url)
+def get_js_page(url):
+    option = Options()
+    option.headless = True
+    driver = webdriver.Firefox(options=option)
+    driver.get(url)
+    http_static_response = driver.execute_script('return document.documentElement.outerHTML')
+    return http_static_response
+
+
+
+def parse_url(url, static_url=True, html_class=False):
+    if static_url:
+        http_doc = get_page(url)
+    else:
+        http_doc = get_js_page(url)
+        
     http_response_info = BeautifulSoup(http_doc, 'html.parser')
+
     return http_response_info
 
-def get_page_links(url):
-    http_response_info = parse_url(url)
+
+def get_page_links(url, static_url=True):
+    http_response_info = parse_url(url, static_url=static_url)
     for link in http_response_info.find_all('a'):
-        yield link#.get('href')
+        yield link
+
 
 def get_page_info(url, html_tag):
     http_response_info = parse_url(url)
-    for item in http_response_info.find_all(html_tag):
+    
+    selected_elements = http_response_info.find_all(html_tag)
+
+    for item in selected_elements:
         yield item
 
 
-def get_bbc_news_fulltext(url, html_tag='a'):
-    http_response_info = parse_url(url)
-    info = ''
-    for paragrafo in http_response_info.find_all('p'):
-        info += paragrafo.get_text() + os.linesep*2
-    info = info.split('Já assistiu aos nossos novos vídeos no YouTube? Inscreva-se no nosso canal!')[0]
-    info = info.split('Estes são links externos e abrirão numa nova janela')[1]
-    return info
+def get_unb_news(target_folder='', encoding='utf8'):
+
+    http_response_unb_noticias = get_page_info(unb_noticias, 'h2')
+    http_response_unb_ciencia = get_page_info(unb_ciencias, 'h2')
+    
+    news_link = []
+    
+    for title in http_response_unb_noticias:
+        title_info = title.get_text().strip()
+        link = unb_noticias+title.find('a').get('href')[1:]
+        news_link.append((title_info, link))
+    
+    for title in http_response_unb_ciencia:
+        if title.find('a'): #.get('href'):
+            title_info = title.get_text().strip()
+            link = unb_ciencias+title.find('a').get('href')[1:]
+            news_link.append((title_info, link))
+    
+    for new in news_link:
+        get_unb_news_fulltext(new[0], new[1])
 
 
-def get_bbc_news(url=bbc_br, html_tag='a', target_folder=''):
-    http_response_info = get_page_info(url, html_tag)
+def get_unb_news_fulltext(title, url):
+    
+    http_response = get_page_info(url, 'p')
+    
+    for i in http_response:
+        print(i.get_text().strip())
+        print()
+    input('PAUSAR....')
+
+
+
+
+def get_bbc_news(url=bbc_br, html_tag='a', target_folder='', encoding='utf8'):
+    #Necessário converter arquivos finais com 'convert_to_readaloud'...
+    
+    http_response_info = get_page_links(url)
 
     n = itertools.count()
 
@@ -81,51 +131,69 @@ def get_bbc_news(url=bbc_br, html_tag='a', target_folder=''):
             ignore_text = False
 
             try:
-                fulltext_data = get_bbc_news_fulltext(link)
+                fulltext_data = get_bbc_news_fulltext(link, encoding=encoding)
             except requests.exceptions.ConnectionError:
                 ignore_text = True
 
-            with open(target_folder+'noticias_bbc_{}.txt'.format(next(n)), 'w') as f:
-                f.write(title+os.linesep)
-                f.write(link+os.linesep*3)
+            with open(target_folder+'noticias_bbc_{}.txt'.format(next(n)), 'wb') as f:
+                nl = os.linesep.encode(encoding)
+                title = title.encode(encoding)
+                link = link.encode(encoding)
+
+                f.write(title+nl)
+                f.write(link+nl*3)
                 if not ignore_text:
                     f.write(fulltext_data)
-            
-            time.sleep(0.2)
+
+
+def charset_fix(text, encoding='utf8'):
+    info = text
+    info = info.replace('—', '---')
+    info = info.replace('“', '"')
+    info = info.replace('”', '"')
+    info = info.replace(' – ', '---')
+    info = info.replace('‘', "'")
+    info = info.replace('’', "'")
+    info = info.replace('…', '...')
+    try:
+        return info.encode(encoding)
+    except UnicodeEncodeError:
+        return info.encode('utf8')
+
+
+
+def get_bbc_news_fulltext(url, html_tag='a', encoding='utf8'):
+    http_response_info = parse_url(url)
+    info = ''
+    for paragrafo in http_response_info.find_all('p'):
+        info += paragrafo.get_text() + os.linesep*2
+    info = info.split('Já assistiu aos nossos novos vídeos no YouTube? Inscreva-se no nosso canal!')[0]
+    info = info.split('Estes são links externos e abrirão numa nova janela')[1]
+    info = charset_fix(info)
+    return info
+
 
 
 def get_folha_news_fulltext(url, title='', html_tag='a', target_folder='', news_num=0):
     http_response_info = parse_url(url)
-    encoding = 'utf8'#http_response_info.original_encoding
+    encoding = 'latin1'
 
     title_info = ''
 
     for t in http_response_info.find_all('h1'):
-        title_info = t.get_text().strip()
+        title_info = t.get_text().strip().encode(encoding)
+
+    url_info = url.encode(encoding)
+    nl = os.linesep.encode(encoding)
+
 
     output_info = []
-    output_info.append(title_info.encode('raw_unicode_escape'))
-
-    info = ''
+    output_info.append(title_info+nl)
+    output_info.append(url_info+nl*3)
 
     for paragrafo in http_response_info.find_all('p'):
 
         p_info = paragrafo.get_text().strip()
-        p_info = p_info.encode('raw_unicode_escape')
-
-        output_info.append(p_info)
-
-        """
-        try:
-            p_info = p_info.encode('raw_unicode_escape')
-            p_info = codecs.decode(p_info, 'utf8')
-        except UnicodeDecodeError:
-            p_info = codecs.decode(p_info, 'latin1')
-            p_info = paragrafo.get_text().strip().encode('raw_unicode_escape')
-            p_info = codecs.decode(p_info, 'utf8')
-        """
-        '''
-        print(p_info)
         re_time_tag = re.search(r'\d\d\.\w\w\w\.\d\d\d\d\s..\s', p_info)
 
         if p_info.find('Assinantes podem liberar 5 acessos por dia para cont') != -1:
@@ -146,7 +214,10 @@ def get_folha_news_fulltext(url, title='', html_tag='a', target_folder='', news_
         elif re_time_tag != None:
             pass
 
-        elif p_info.find('Copyright Folha de S.Paulo. Todos os direitos reservados.') != -1:
+        elif p_info.find('Copyright') != -1:
+            break
+
+        elif re.search(r'Os coment*rios n*o representam a opini*o do jornal', p_info) != None:
             break
         
         elif p_info.find('vantagens de ser assinante da Folha?') != -1:
@@ -156,49 +227,21 @@ def get_folha_news_fulltext(url, title='', html_tag='a', target_folder='', news_
             break
         
         else:
-            info += p_info
-            #output_info.append(p_info)
-        '''
-    """
-    while True:
-        if info.find('\n\n\n') != -1: 
-            info = info.replace('\n\n\n', '\n\n')
-            info = info.replace('Gostaria de receber as principais notícias do Brasil e do mundo?','')
-            info = info.replace('Assinantes podem liberar 5 acessos por dia para conteúdos da Folha','')
-            info = info.replace('Assinantes podem liberar 5 acessos por dia para conteúdos da Folha','')
-        else: break
-    """
-    #info_b = bytes(info, encoding)
-    #output_info.append(info_b)
-
-    print(info)
-
+            p_info = charset_fix(p_info, encoding=encoding)
+            if len(p_info) < 15:
+                pass
+            else:
+                output_info.append(p_info+nl*2)
     
     with open(target_folder+'noticias_folha_{}.txt'.format(news_num), 'wb') as f:
         for line in output_info:
             f.write(line)
     
-    """
-    with open(target_folder+'noticias_folha_{}.txt'.format(news_num), 'w', encoding=encoding) as f:
-        f.write(title_info+os.linesep*2)
-        f.write(info)
-    """
-    '''
-    o = []
-    with open(target_folder+'noticias_folha_{}.txt'.format(news_num), 'rb') as f:
-        for line in f.readlines():
-            s = codecs.decode(line, encoding='utf-8')
-            o.append(s)
-    
-    for i in o:
-        print(i)
-    '''
-    
-    input('CTRL+C')
 
-
-
+   
 def get_folha_news(url=folha, html_tag='h2', target_folder=''):
+    #Necessário converter arquivos finais com 'convert_to_readaloud'...
+
     http_response_info = get_page_info(url, html_tag)
 
     n = itertools.count()
@@ -255,32 +298,90 @@ def get_correioweb_news(target_folder=''):
     print(news_links)
 
 
-
-def get_br_de_fato_news(url=br_de_fato, target_folder=''):
-    http_response = get_page_links(url)
-
+def grab_and_decode(
+        http_response,\
+        encoding,\
+        file_label='noticias',\
+        breakers=[],\
+        label_location='parent', #child, inside...
+        label_tag='h3',\
+        target_folder=''):
+    
     n = itertools.count()
 
     news_links = []
 
     for ln in http_response:
-        if ln.parent.find('h3'):
-            titulo = ln.parent.find('h3').get_text().strip()
+        if label_location == 'parent':
+            if ln.parent.find(label_tag):
+                titulo = charset_fix(ln.parent.find(label_tag).get_text().strip(), encoding=encoding)
+                link = ln.get('href').strip()
+                news_links.append((titulo, link))
+
+        elif label_location == 'child': 
+            if ln.find(label_tag):
+                titulo = charset_fix(ln.find(label_tag).get_text().strip(), encoding=encoding)
+                link = ln.get('href').strip()
+                news_links.append((titulo, link))
+        
+        elif label_location == 'inside':
+            titulo = charset_fix(ln.get('title').strip().strip(), encoding=encoding)
             link = ln.get('href').strip()
             news_links.append((titulo, link))
+
+    print(news_links)
     
     for new in news_links:
-        with open(target_folder+'noticias_br_de_fato_{}.txt'.format(next(n)), 'w') as f:
-            f.write(new[0]+os.linesep)
-            f.write(new[1]+os.linesep*3)
+        with open(target_folder+'{file_label}_{n}.txt'.format(file_label=file_label, n=next(n)), 'wb') as f:
+            nl = os.linesep.encode(encoding)
+            link_byte = new[1].encode(encoding)
+            f.write(new[0]+nl)
+            f.write(link_byte+nl*3)
             new_info = parse_url(new[1])
             for paragrafo in new_info.find_all('p'):
-                if paragrafo.get_text().strip() == 'Quem Somos':
+                if paragrafo.get_text().strip() in breakers:
                     break
+                elif len(paragrafo.get_text().strip()) < 15:
+                    pass
                 else:
-                    f.write(paragrafo.get_text()+os.linesep*2)
+                    paragrafo_info = charset_fix(paragrafo.get_text().strip(), encoding=encoding)
+                    f.write(paragrafo_info+nl*2)
 
     return news_links
+
+
+
+def get_br_de_fato_news(url=br_de_fato, target_folder=''):
+    #Não necessário converter arquivos finais...
+
+    http_response = get_page_links(url)
+    breakers=['Quem Somos']
+    target_folder=target_folder
+    label_tag='h3'
+    file_label='noticias_br_d_fato'
+
+    grab_and_decode(http_response,\
+        'latin1',\
+        breakers=breakers,\
+        target_folder=target_folder,\
+        label_tag=label_tag,\
+        file_label=file_label)
+
+
+def get_bbc2_news(url=bbc_br, target_folder=''):
+    http_response = get_page_links(url)
+    breakers=['Quem Somos']
+    target_folder=target_folder
+    label_tag='h3'
+    file_label='noticias_bbc_br'
+
+    grab_and_decode(http_response,\
+        'utf8',\
+        breakers=breakers,\
+        target_folder=target_folder,\
+        label_tag=label_tag,\
+        file_label=file_label)
+
     
 
 def procurar_no_portal_da_transparencia(cpf):
